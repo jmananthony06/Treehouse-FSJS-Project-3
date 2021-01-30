6d54f8fd217829767c6878452d861270769b9704
jmananthony06 <jmananthony06@gmail.com>
2021-01-29 20:38:37 -0500

    another commit
    js updates to jquery file

--- js/index.js
+++ js/index.js
@@ -31,18 +31,69 @@ $("input:checkbox").change(function(e) {
         $(`*[data-day-and-time="${schedule}"]`).parent().show();
     }
 });
+//on page load, everything is hidden by default
+$("#credit-card, #paypal, #bitcoin").hide();
 
+//function allows for each payment to show upon selection
 $("#payment").change(function(e) {
     let value = e.target.value;
-    $("#credit-card, #paypal, #bitcoin").show();
+    $("#credit-card, #paypal, #bitcoin").hide();
     if(value === 'credit-card') {
-        $("#paypal, #bitcoin").hide();
+        $("#credit-card").show();
     } else if(value === 'bitcoin') {
-        $("#credit-card, #paypal").hide();
+        $("#bitcoin").show();
     } else if(value === 'paypal'){
-        $("#credit-card, #bitcoin").hide();
+        $("#paypal").show();
     }
 });
+
+$('#submitButton').click(function(e) {
+    e.preventDefault(); //stops default action of submit button 
+    let dataElements = $('#name, #mail, #title, #size, #design, #color');
+    dataElements.each(function(){
+        let el = $(this);
+        let id = el.attr("id"); //what the id is
+        let val = el.val(); //what the user wrote
+        let validator = validationObj[id]; // accessing piece of an object 
+        validator ? validator(val, el) : null; // out of the data, get value of id and get into line 64 object to see if property matches the id of the thing we found. If it exists, call function with value via ternerary operator
+        console.log($(this).attr("id"));
+        console.log($(this).val());
+    });
+});
+
+//called on line 57
+// if there's a string with a certain length, good, but if no length at all, shoot error
+let validationObj = {
+    name: function(val, el){
+        console.log("checking to see if is valid");
+        if(typeof val == "string" && val.length > 0){
+            el.removeClass('error');
+        } else {
+            el.addClass('error');
+        }
+    }, 
+    mail: function(val, el){
+        console.log("checking to see if mail is valid");
+//         bool isValid = false;
+
+// try
+// {
+//     MailAddress address = new MailAddress(emailAddress);
+//     isValid = (address.Address == emailAddress);
+//     // or
+//     // isValid = string.IsNullOrEmpty(address.DisplayName);
+// }
+// catch (FormatException)
+// {
+//     // address is invalid
+}
+        if(typeof val == "string" && val.length > 0){
+            el.removeClass('error');
+        } else {
+            el.addClass('error');
+        }
+    }
+}
 
 
 
